7f6f4a8ff34d51f4ec8781dff6fb7b28834b3357
FrankQiu <iam.frankqiu@gmail.com>
2023-06-08 22:37:29 +0800

    feat: finish storage code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
+export const getFromLocalStorage = <T, K extends string>(
+  key: K,
+  defaultValue?: T
+): T | null => {
+  try {
+    const data = window?.localStorage.getItem(key);
+    return data ? JSON.parse(data) : defaultValue;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+};
 
+export const setToLocalStorage = <T, K extends string>(key: K, value: T) => {
+  try {
+    window?.localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const removeFromLocalStorage = <T extends string>(key: T) => {
+  try {
+    window?.localStorage.removeItem(key);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const clearLocalStorage = () => {
+  try {
+    window?.localStorage.clear()
+  } catch (error) {
+    
+  }
+}
